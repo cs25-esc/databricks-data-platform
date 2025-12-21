f34082fbe209ed3777b81d33bb6d62c5a169b46a
cs25-esc <charanarc3@gmail.com>
2025-12-21 22:20:52 +0530

    new merge check

--- notebooks/customers_bronze_to_silver_new.py
+++ notebooks/customers_bronze_to_silver_new.py
@@ -53,7 +53,7 @@ SELECT
 FROM df_bronze_hash_vw v
 LEFT JOIN training_catalog.silver.customers_dim d
 ON v.customer_id = d.customer_id 
-AND d.is_current() is true
+AND d.is_current is true
 AND (h.hash_value <> d.hash_value OR d.customer_id is null)
 """)
 
