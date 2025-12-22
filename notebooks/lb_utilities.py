0dfe8ad54113a916c6ecc556ab3ae0273dba7b2d
Charan Sai Annam <68850280+cs25-esc@users.noreply.github.com>
2025-12-22 11:26:34 +0530

    Feature/databricks asset bundle (#5)
    * add all necessary files
    
    * add databricks.yml
    
    * a
    
    * a
    
    * a
    
    * added include in databricks.yml file
    
    * added include in databricks.yml file
    
    * added include in databricks.yml file
    
    * added include in databricks.yml file
    
    * added include in databricks.yml file
    
    * added include in databricks.yml file
    
    * added customer flow
    
    * added
    
    * added utils3.py
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added notebooks
    
    * added orders landing to silver table
    
    * fixed  orders landing to silver table script
    
    * improved logging and few changes
    
    * file_name UC metadata.file_name added
    
    * fix count logging
    
    * added query awaits termination step
    
    * new merge check
    
    * new merge check
    
    * new merge check
    
    * new merge check
    
    * new merge check
    
    * new merge check
    
    * new merge check
    
    * new merge check
    
    * new merge check
    
    * new scd2 logic added into main scripts

--- notebooks/lb_utilities.py
+++ notebooks/lb_utilities.py
@@ -2,3 +2,9 @@ from pyspark.sql.functions import *
 
 def add_loadts(df):
     return df.withColumn("load_ts", current_timestamp())
+
+
+def add_hash_column(df, col_list):
+    df = df.withColumn("hash_value" , sha2(concat_ws("||" , *[col(c) for c in col_list]), 0))
+
+    return df
